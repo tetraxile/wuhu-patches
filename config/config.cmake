188ca2a1e9a46cb1be978780ccdd2100d4cc6c7f
fruityloops1 <fruityloops@littun.co>
2025-02-17 18:19:36 +0100

    nofast

--- config/config.cmake
+++ config/config.cmake
@@ -2,7 +2,7 @@ set(LINKFLAGS -nodefaultlibs)
 set(LLDFLAGS --no-demangle --gc-sections)
 
 set(OPTIMIZE_OPTIONS_DEBUG -O2 -gdwarf-4)
-set(OPTIMIZE_OPTIONS_RELEASE -Ofast -flto)
+set(OPTIMIZE_OPTIONS_RELEASE -O3 -ffast-math -flto)
 set(WARN_OPTIONS -Werror=return-type)
 
 set(DEFINITIONS "")
