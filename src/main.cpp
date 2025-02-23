ee31feb131473af77542a27b2348ca6938a61d41
fruityloops1 <fruityloops@littun.co>
2025-02-24 00:55:28 +0100

    dfgdf

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "al/Library/Controller/InputFunction.h"
 #include "al/Library/LiveActor/ActorPoseKeeper.h"
 #include "al/Library/LiveActor/LiveActor.h"
+#include "hk/gfx/DebugRenderer.h"
 #include "hk/hook/Trampoline.h"
+
+#include "agl/common/aglDrawContext.h"
+#include "al/Library/System/GameSystemInfo.h"
+#include "game/System/Application.h"
+#include "game/System/GameSystem.h"
 
 HkTrampoline<void, al::LiveActor*> marioControl = hk::hook::trampoline([](al::LiveActor* player) -> void {
     if (al::isPadHoldA(-1)) {
@@ -12,6 +18,28 @@ HkTrampoline<void, al::LiveActor*> marioControl = hk::hook::trampoline([](al::Li
     marioControl.orig(player);
 });
 
+HkTrampoline<void, GameSystem*> drawMainHook = hk::hook::trampoline([](GameSystem* gameSystem) -> void {
+    drawMainHook.orig(gameSystem);
+
+    auto* drawContext = Application::instance()->mDrawSystemInfo->drawContext;
+
+    hk::gfx::DebugRenderer::instance()->clear();
+    hk::gfx::DebugRenderer::instance()->begin(drawContext->getCommandBuffer()->ToData()->pNvnCommandBuffer);
+    /// hk::gfx::DebugRenderer::instance()->drawTest();
+    hk::gfx::DebugRenderer::instance()->drawQuad(
+        { { 0.0, 0.0 }, { 0, 0 }, 0xFF0000FF },
+        { { 0.5, 0.0 }, { 1.0, 0 }, 0xFF00FF00 },
+        { { 0.5, 1.0 }, { 1.0, 1.0 }, 0xFFFFFF00 },
+        { { 0.0, 1.0 }, { 0, 1.0 }, 0xFFFF0000 });
+    /*hk::gfx::DebugRenderer::instance()->drawTri(
+        { { 0.5, 0.0 }, { 0, 0 }, 0xFF00FF00 },
+        { { 1.0, 0.0 }, { 0, 0 }, 0x550000FF },
+        { { 1.0, 1.0 }, { 0, 0 }, 0xFFFF0000 });*/
+    hk::gfx::DebugRenderer::instance()->end();
+});
+
 extern "C" void hkMain() {
     marioControl.installAtSym<"_ZN19PlayerActorHakoniwa7controlEv">();
+    drawMainHook.installAtSym<"_ZN10GameSystem8drawMainEv">();
+    hk::gfx::DebugRenderer::instance()->installHooks();
 }
