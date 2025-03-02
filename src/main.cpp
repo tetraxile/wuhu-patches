bb35cd58648580ffb0e7f8dbc46d5035c46ee657
fruityloops1 <fruityloops@littun.co>
2025-03-03 00:56:04 +0100

    insane2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
-#include "al/Library/Controller/InputFunction.h"
-#include "al/Library/LiveActor/ActorPoseKeeper.h"
-#include "al/Library/LiveActor/LiveActor.h"
 #include "hk/gfx/DebugRenderer.h"
 #include "hk/hook/Trampoline.h"
 
 #include "agl/common/aglDrawContext.h"
-#include "al/Library/System/GameSystemInfo.h"
+
+#include "game/Sequence/HakoniwaSequence.h"
 #include "game/System/Application.h"
 #include "game/System/GameSystem.h"
+
+#include "al/Library/Controller/InputFunction.h"
+#include "al/Library/LiveActor/ActorPoseKeeper.h"
+#include "al/Library/LiveActor/ActorPoseUtil.h"
+#include "al/Library/LiveActor/LiveActor.h"
+#include "al/Library/LiveActor/LiveActorKit.h"
+#include "al/Library/Player/PlayerHolder.h"
+#include "al/Library/Scene/Scene.h"
+#include "al/Library/System/GameSystemInfo.h"
 
 HkTrampoline<void, al::LiveActor*> marioControl = hk::hook::trampoline([](al::LiveActor* player) -> void {
     if (al::isPadHoldA(-1)) {
         player->getPoseKeeper()->getVelocityPtr()->y = 0;
-        al::getTransPtr(player)->y += 100;
+        al::getTransPtr(player)->y += 20;
     }
 
     marioControl.orig(player);
@@ -23,21 +30,39 @@ HkTrampoline<void, GameSystem*> drawMainHook = hk::hook::trampoline([](GameSyste
 
     auto* drawContext = Application::instance()->mDrawSystemInfo->drawContext;
 
-    hk::gfx::DebugRenderer::instance()->clear();
-    hk::gfx::DebugRenderer::instance()->begin(drawContext->getCommandBuffer()->ToData()->pNvnCommandBuffer);
-    /// hk::gfx::DebugRenderer::instance()->drawTest();
-    hk::gfx::DebugRenderer::instance()->drawQuad(
-        { { 350, 350 }, { 0, 0 }, 0x7f000000 },
-        { { 980, 350 }, { 1.0, 0 }, 0x7f000000 },
-        { { 980, 680 }, { 1.0, 1.0 }, 0x7f000000 },
-        { { 350, 680 }, { 0, 1.0 }, 0x7f000000 });
-    hk::gfx::DebugRenderer::instance()->drawString({ 400, 400 }, "meow :3", 0xff0000ff);
-    hk::gfx::DebugRenderer::instance()->drawString({ 400, 430 }, "ABCDEFGHIJKLMNO\nabcdefghijklmno\n@balls@@**/", 0xffffffff);
-    /*hk::gfx::DebugRenderer::instance()->drawTri(
-        { { 0.5, 0.0 }, { 0, 0 }, 0xFF00FF00 },
-        { { 1.0, 0.0 }, { 0, 0 }, 0x550000FF },
-        { { 1.0, 1.0 }, { 0, 0 }, 0xFFFF0000 });*/
-    hk::gfx::DebugRenderer::instance()->end();
+    HakoniwaSequence* sequence = static_cast<HakoniwaSequence*>(gameSystem->mSequence);
+    if (sequence == nullptr)
+        return;
+
+    al::Scene* scene = sequence->mCurrentScene;
+    al::LiveActor* player = nullptr;
+    if (scene && scene->mLiveActorKit && scene->mLiveActorKit->mPlayerHolder)
+        player = scene->mLiveActorKit->mPlayerHolder->tryGetPlayer(0);
+
+    auto* renderer = hk::gfx::DebugRenderer::instance();
+
+    renderer->clear();
+    renderer->begin(drawContext->getCommandBuffer()->ToData()->pNvnCommandBuffer);
+
+    renderer->setGlyphSize(0.45);
+
+    renderer->drawQuad(
+        { { 30, 30 }, { 0, 0 }, 0xef000000 },
+        { { 300, 30 }, { 1.0, 0 }, 0xef000000 },
+        { { 300, 100 }, { 1.0, 1.0 }, 0xef000000 },
+        { { 30, 100 }, { 0, 1.0 }, 0xef000000 });
+
+    renderer->setCursor({ 50, 50 });
+
+    if (player) {
+        const sead::Vector3f& trans = al::getTrans(player);
+
+        renderer->printf("Pos: %.2f %.2f %.2f\n", trans.x, trans.y, trans.z);
+    } else {
+        renderer->printf("No player\n");
+    }
+
+    renderer->end();
 });
 
 extern "C" void hkMain() {
