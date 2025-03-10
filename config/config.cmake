a71778a5f12fb1ce17ca3709378dac60e7f184f1
fruityloops1 <fruityloops@littun.co>
2025-03-10 01:02:33 +0100

    Config update

--- config/config.cmake
+++ config/config.cmake
@@ -3,7 +3,7 @@ set(LLDFLAGS --no-demangle --gc-sections)
 
 set(OPTIMIZE_OPTIONS_DEBUG -O2 -gdwarf-4)
 set(OPTIMIZE_OPTIONS_RELEASE -O3 -ffast-math -flto)
-set(WARN_OPTIONS -Werror=return-type)
+set(WARN_OPTIONS -Werror=return-type -Wno-invalid-offsetof)
 
 set(INCLUDES include include/agl)
 
@@ -13,6 +13,7 @@ set(CXX_OPTIONS "")
 set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
 
+set(IS_32_BIT FALSE)
 set(TARGET_IS_STATIC FALSE)
 set(MODULE_NAME Hakkun-Example)
 set(TITLE_ID 0x0100000000010000)
@@ -23,4 +24,4 @@ set(USE_SAIL TRUE)
 set(TRAMPOLINE_POOL_SIZE 0x10)
 set(BAKE_SYMBOLS FALSE)
 
-set(HAKKUN_ADDONS DebugRenderer)
+set(HAKKUN_ADDONS Nvn DebugRenderer)
